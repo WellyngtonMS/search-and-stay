c84cd3b4eb0c0a3502d885b82ae732ea9758905a
Wellyngton Matheus <wellyngtoncatarino@gmail.com>
2023-06-01 06:02:19 -0300

    feat: notification component

---
+++ components/Snackbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-snackbar
+		v-model="showSnackbar"
+		timeout="2500"
+		centered
+		top
+		:color="snackbarColor"
+		transition="slide-y-reverse-transition"
+		>
+			{{ snackbarText }}
+	</v-snackbar>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            showSnackbar: false,
+            snackbarText: '',
+			snackbarColor: ''
+        }
+    },
+    created() {
+        this.$store.subscribe((mutation, state) => {
+            if (mutation.type === 'snackbar/showMessage') {
+                this.snackbarText = state.snackbar.content
+                this.snackbarColor = state.snackbar.color
+                this.showSnackbar = true
+            }
+        })
+    }
+}
+</script>
