3776ddee8451ca062952cf43f1a91f814805b91a
Wellyngton Matheus <wellyngtoncatarino@gmail.com>
2023-06-01 11:17:53 -0300

    feat: card component for exhibition of items

---
+++ components/Cards/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-card>
+        <v-card-text>
+            <div>{{ name }}</div>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn color="primary" @click="editItem">Editar</v-btn>
+            <v-btn color="error" @click="deleteItem">Excluir</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        },
+    },
+    computed: {
+        name() {
+            return this.item.name;
+        },
+    },
+    methods: {
+        editItem() {
+            // Lógica para editar o item
+        },
+        deleteItem() {
+            // Lógica para excluir o item
+        },
+    },
+};
+</script>
